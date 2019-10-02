35c292c932c2f7f5e67be237f2aaf72be0e5f5ec
Relintai <relintai@gmail.com>
2019-10-02 11:43:36 +0200

    Added the missing get_loot binding into LootDataBase.

--- loot/loot_data_base.cpp
+++ loot/loot_data_base.cpp
@@ -19,6 +19,8 @@ LootDataBase::LootDataBase() {
 void LootDataBase::_bind_methods() {
 	BIND_VMETHOD(MethodInfo("_get_loot", PropertyInfo(Variant::ARRAY, "into")));
 
+	ClassDB::bind_method(D_METHOD("get_loot"), &LootDataBase::get_loot);
+
 	ClassDB::bind_method(D_METHOD("get_chance"), &LootDataBase::get_chance);
 	ClassDB::bind_method(D_METHOD("set_chance", "value"), &LootDataBase::set_chance);
 	ADD_PROPERTY(PropertyInfo(Variant::INT, "chance"), "set_chance", "get_chance");
