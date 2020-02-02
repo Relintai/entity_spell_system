1704caafc8a12afc1e12786125f5935d246c4fd4
Relintai <relintai@gmail.com>
2020-02-02 18:17:22 +0100

    Added more properties for EntityCreateInfo.

--- utility/entity_create_info.cpp
+++ utility/entity_create_info.cpp
@@ -21,3 +21,136 @@ SOFTWARE.
 */
 
 #include "entity_create_info.h"
+
+
+int EntityCreateInfo::get_guid() const { 
+    return _guid; 
+}
+void EntityCreateInfo::set_guid(const int value) { 
+    _guid = value; 
+}
+
+int EntityCreateInfo::get_class_id() const {
+    return _class_id;
+}
+void EntityCreateInfo::set_class_id(const int value) {
+    _class_id = value;
+}
+
+int EntityCreateInfo::get_network_owner() const {
+    return _network_owner;
+}
+void EntityCreateInfo::set_network_owner(const int value) {
+    _network_owner = value;
+}
+
+EntityEnums::EntityController EntityCreateInfo::get_entity_controller() const { 
+    return _entity_controller; 
+}
+void EntityCreateInfo::set_entity_controller(const EntityEnums::EntityController value) { 
+    _entity_controller = value; 
+}
+
+String EntityCreateInfo::get_entity_name() const { 
+    return _entity_name; 
+}
+void EntityCreateInfo::set_entity_name(const String &value) { 
+    _entity_name = value; 
+}
+
+String EntityCreateInfo::get_node_name() const { 
+    return _entity_name; 
+}
+void EntityCreateInfo::set_node_name(const String &value) { 
+    _entity_name = value; 
+}
+
+int EntityCreateInfo::get_level() const { 
+    return _level;
+}
+void EntityCreateInfo::set_level(const int value) { 
+    _level = value;
+ }
+
+int EntityCreateInfo::get_xp() const { 
+    return _xp; 
+}
+void EntityCreateInfo::set_xp(const int value) { 
+    _xp = value; 
+}
+
+Transform EntityCreateInfo::get_transform() const {
+    return _transform;
+}
+void EntityCreateInfo::set_transform(const Transform &value) {
+    _transform =  value;
+}
+
+Transform2D EntityCreateInfo::get_transform2d() const {
+    return _transform2d;
+}
+void EntityCreateInfo::set_transform2d(const Transform2D &value) {
+    _transform2d = value;
+}
+
+Ref<EntityData> EntityCreateInfo::get_entity_data() const { 
+    return _entity_data; 
+}
+void EntityCreateInfo::set_entity_data(const Ref<EntityData> &value) { 
+    _entity_data = value; 
+}
+
+EntityCreateInfo::EntityCreateInfo() {
+    _guid = 0;
+    _class_id = 0;
+    _network_owner = 0;
+	_entity_controller = EntityEnums::ENITIY_CONTROLLER_NONE;
+	_level = 0;
+	_xp = 0;
+}
+
+EntityCreateInfo::~EntityCreateInfo() {
+	_entity_data.unref();
+}
+
+void EntityCreateInfo::_bind_methods() {
+		ClassDB::bind_method(D_METHOD("get_entity_controller"), &EntityCreateInfo::get_entity_controller);
+		ClassDB::bind_method(D_METHOD("set_entity_controller", "value"), &EntityCreateInfo::set_entity_controller);
+		ADD_PROPERTY(PropertyInfo(Variant::INT, "entity_controller", PROPERTY_HINT_ENUM, EntityEnums::BINDING_STRING_ENTITY_CONTOLLER), "set_entity_controller", "get_entity_controller");
+
+        ClassDB::bind_method(D_METHOD("get_class_id"), &EntityCreateInfo::get_class_id);
+		ClassDB::bind_method(D_METHOD("set_class_id", "value"), &EntityCreateInfo::set_class_id);
+		ADD_PROPERTY(PropertyInfo(Variant::INT, "class_id", PROPERTY_HINT_ENUM, EntityEnums::BINDING_STRING_ENTITY_CONTOLLER), "set_class_id", "get_class_id");
+
+        ClassDB::bind_method(D_METHOD("get_network_owner"), &EntityCreateInfo::get_network_owner);
+		ClassDB::bind_method(D_METHOD("set_network_owner", "value"), &EntityCreateInfo::set_network_owner);
+		ADD_PROPERTY(PropertyInfo(Variant::INT, "network_owner", PROPERTY_HINT_ENUM, EntityEnums::BINDING_STRING_ENTITY_CONTOLLER), "set_network_owner", "get_network_owner");
+
+		ClassDB::bind_method(D_METHOD("get_entity_name"), &EntityCreateInfo::get_entity_name);
+		ClassDB::bind_method(D_METHOD("set_entity_name", "value"), &EntityCreateInfo::set_entity_name);
+		ADD_PROPERTY(PropertyInfo(Variant::STRING, "player_name"), "set_entity_name", "get_entity_name");
+
+        ClassDB::bind_method(D_METHOD("get_node_name"), &EntityCreateInfo::get_node_name);
+		ClassDB::bind_method(D_METHOD("set_node_name", "value"), &EntityCreateInfo::set_node_name);
+		ADD_PROPERTY(PropertyInfo(Variant::STRING, "node_name"), "set_node_name", "get_node_name");
+
+		ClassDB::bind_method(D_METHOD("get_level"), &EntityCreateInfo::get_level);
+		ClassDB::bind_method(D_METHOD("set_level", "value"), &EntityCreateInfo::set_level);
+		ADD_PROPERTY(PropertyInfo(Variant::INT, "level"), "set_level", "get_level");
+
+		ClassDB::bind_method(D_METHOD("get_xp"), &EntityCreateInfo::get_xp);
+		ClassDB::bind_method(D_METHOD("set_xp", "value"), &EntityCreateInfo::set_xp);
+		ADD_PROPERTY(PropertyInfo(Variant::INT, "xp"), "set_xp", "get_xp");
+
+        ClassDB::bind_method(D_METHOD("get_transform"), &EntityCreateInfo::get_transform);
+		ClassDB::bind_method(D_METHOD("set_transform", "value"), &EntityCreateInfo::set_transform);
+		ADD_PROPERTY(PropertyInfo(Variant::TRANSFORM, "transform"), "set_transform", "get_transform");
+
+        ClassDB::bind_method(D_METHOD("get_transform2d"), &EntityCreateInfo::get_transform2d);
+		ClassDB::bind_method(D_METHOD("set_transform2d", "value"), &EntityCreateInfo::set_transform2d);
+		ADD_PROPERTY(PropertyInfo(Variant::TRANSFORM2D, "transform2d"), "set_transform2d", "get_transform2d");
+
+		ClassDB::bind_method(D_METHOD("get_entity_data"), &EntityCreateInfo::get_entity_data);
+		ClassDB::bind_method(D_METHOD("set_entity_data", "value"), &EntityCreateInfo::set_entity_data);
+		ADD_PROPERTY(PropertyInfo(Variant::OBJECT, "entity_data", PROPERTY_HINT_RESOURCE_TYPE, "EntityData"), "set_entity_data", "get_entity_data");
+	}
--- utility/entity_create_info.h
+++ utility/entity_create_info.h
@@ -32,53 +32,56 @@ class EntityCreateInfo : public Reference {
 	GDCLASS(EntityCreateInfo, Reference);
 
 public:
-	int get_guid() { return _guid; }
-	void set_guid(int value) { _guid = value; }
+	int get_guid() const;
+	void set_guid(const int value);
 
-	EntityEnums::EntityController get_entity_controller() { return _entity_controller; }
-	void set_entity_controller(EntityEnums::EntityController value) { _entity_controller = value; }
+	int get_class_id() const;
+	void set_class_id(const int value);
 
-	String get_entity_name() { return _entity_name; }
-	void set_entity_name(String value) { _entity_name = value; }
+	int get_network_owner() const;
+	void set_network_owner(const int value);
 
-	int get_level() { return _level; }
-	void set_level(int value) { _level = value; }
+	EntityEnums::EntityController get_entity_controller() const;
+	void set_entity_controller(const EntityEnums::EntityController value);
 
-	int get_xp() { return _xp; }
-	void set_xp(int value) { _xp = value; }
+	String get_entity_name() const;
+	void set_entity_name(const String &value);
 
-	Ref<EntityData> get_entity_data() { return _entity_data; }
-	void set_entity_data(Ref<EntityData> value) { _entity_data = value; }
+	String get_node_name() const;
+	void set_node_name(const String &value);
+
+	int get_level() const;
+	void set_level(const int value);
+
+	int get_xp() const;
+	void set_xp(const int value);
+
+	Transform get_transform() const;
+	void set_transform(const Transform &value);
+
+	Transform2D get_transform2d() const;
+	void set_transform2d(const Transform2D &value);
+
+	Ref<EntityData> get_entity_data() const;
+	void set_entity_data(const Ref<EntityData> &value);
+
+	EntityCreateInfo();
+	~EntityCreateInfo();
 
 protected:
-	static void _bind_methods() {
-		ClassDB::bind_method(D_METHOD("get_entity_controller"), &EntityCreateInfo::get_entity_controller);
-		ClassDB::bind_method(D_METHOD("set_entity_controller", "value"), &EntityCreateInfo::set_entity_controller);
-		ADD_PROPERTY(PropertyInfo(Variant::INT, "entity_controller", PROPERTY_HINT_ENUM, EntityEnums::BINDING_STRING_ENTITY_CONTOLLER), "set_entity_controller", "get_entity_controller");
-
-		ClassDB::bind_method(D_METHOD("get_entity_name"), &EntityCreateInfo::get_entity_name);
-		ClassDB::bind_method(D_METHOD("set_entity_name", "value"), &EntityCreateInfo::set_entity_name);
-		ADD_PROPERTY(PropertyInfo(Variant::STRING, "player_name"), "set_entity_name", "get_entity_name");
-
-		ClassDB::bind_method(D_METHOD("get_level"), &EntityCreateInfo::get_level);
-		ClassDB::bind_method(D_METHOD("set_level", "value"), &EntityCreateInfo::set_level);
-		ADD_PROPERTY(PropertyInfo(Variant::STRING, "level"), "set_level", "get_level");
-
-		ClassDB::bind_method(D_METHOD("get_xp"), &EntityCreateInfo::get_xp);
-		ClassDB::bind_method(D_METHOD("set_xp", "value"), &EntityCreateInfo::set_xp);
-		ADD_PROPERTY(PropertyInfo(Variant::STRING, "xp"), "set_xp", "get_xp");
-
-		ClassDB::bind_method(D_METHOD("get_entity_data"), &EntityCreateInfo::get_entity_data);
-		ClassDB::bind_method(D_METHOD("set_entity_data", "value"), &EntityCreateInfo::set_entity_data);
-		ADD_PROPERTY(PropertyInfo(Variant::OBJECT, "entity_data", PROPERTY_HINT_RESOURCE_TYPE, "EntityData"), "set_entity_data", "get_entity_data");
-	}
+	static void _bind_methods();
 
 private:
 	int _guid;
+	int _class_id;
+	int _network_owner;
 	EntityEnums::EntityController _entity_controller;
 	String _entity_name;
 	int _level;
 	int _xp;
+
+	Transform _transform;
+	Transform2D _transform2d;
 
 	Ref<EntityData> _entity_data;
 };
