3c334566ff05a74e913cd5c5ff38ae45aba5f5d2
Relintai <relintai@protonmail.com>
2022-01-12 21:54:38 +0100

    Fix an another copyright header.

--- editor/ess_editor_plugin.cpp
+++ editor/ess_editor_plugin.cpp
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2020 Péter Magyar
+Copyright (c) 2020-2022 Péter Magyar
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
